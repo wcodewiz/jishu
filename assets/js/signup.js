4e05fd86554cf1d1991c4525d19ebad107a8c40b
wcodewiz <samuelclinton975@gmail.com>
2023-09-05 11:43:09 +0100

    wcodewiz

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -4,7 +4,27 @@
 
 
 const validateInput = (element) => {
-    alert(element.value)
+    const errorText = element.parentNode.nextElementSibling;
+    const type = element.type;
+    let errors = "";
+    if (type === "text") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    } else if (type == "email") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`)
+        if (errors === "")
+            errors = email(element.value, "invalid email address");
+    } else {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    }
+    if (errors !== "") {
+        errorText.innerHTML = errors;
+        errorText.classList.remove("error_text");
+        errorText.classList.add("error_text-shown");
+    } else {
+        errorText.innerHTML = "";
+        errorText.classList.add("error_text");
+        errorText.classList.remove("error_text-shown");
+    }
 }
 
 onMount(() => {
@@ -13,5 +33,4 @@ onMount(() => {
     });
 
 });
-
 
