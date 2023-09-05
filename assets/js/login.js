57fdbd7f0166eb0d10ad072285d042ebe5294661
techwolf001 <fawasrufai999@gmail.com>
2023-09-05 13:03:27 +0100

    techwolf001

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,48 @@
-
-const validateInput = (element) => {
+const validateInput = (element, index, query) => {
     const errorText = element.parentNode.nextElementSibling;
-    
     const type = element.type;
     let errors = "";
-    if (type === "email") {
+    if (type === "text") {
         errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
-    }else if (type == "password") {
+    } else if (type == "email") {
         errors = required(element.value, `${element.placeholder ?? "this field"} is required`)
-        if (errors === ""){
+        if (errors === "")
             errors = email(element.value, "invalid email address");
-    } else {
+    } else if (type === "password") {
+        let next = query[++index];
+        if (next === undefined) {
+            next = query[--index]
+        } else {
+            if (next.type === undefined)
+                next = query[--index]
+        }
+
+
+        let error2 = required(next.value, `${next.placeholder ?? "this field"} is required`);
+        if (error2 == "")
+            error2 = same(element.value, next.value, `${next.placeholder ?? "this field"} mismatch`);
+
+        const errorText2 = next.parentNode.nextElementSibling;
+
+        if (error2 !== "") {
+            errorText2.innerHTML = error2;
+            errorText2.classList.remove("error_text");
+            errorText2.classList.add("error_text-shown");
+        } else {
+            errorText2.innerHTML = "";
+            errorText2.classList.add("error_text");
+            errorText2.classList.remove("error_text-shown");
+        }
+
+
+
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+        if (errors == "")
+            errors = min(element.value, 5, `${element.placeholder ?? "this field"} must be at least 5 characters`)
+    }
+    else {
         errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
     }
-}
     if (errors !== "") {
         errorText.innerHTML = errors;
         errorText.classList.remove("error_text");
@@ -26,8 +55,22 @@ const validateInput = (element) => {
 }
 
 onMount(() => {
-    $(".login input", (element, index) => {
-        element.addEventListener("keydown", (ev) => validateInput(element));
+    $(".login input, .login select", (element, index, query) => {
+        element.addEventListener("keydown", (ev) => validateInput(element, index, query));
     });
+
+    $(`button[type*="submit"]`, (element) => {
+        element.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            $(".login input, .login select", (element, index, query) => {
+                validateInput(element, index, query);
+            });
+            return false;
+        });
+
+
+
+    });
+
 
 });
