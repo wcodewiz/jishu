e5d3d616f4178f51bb98f5e2f672c7ccf5da9607
wcodewiz <samuelclinton975@gmail.com>
2023-09-05 12:35:25 +0100

    wcodewiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,14 @@ const onMount = (callback) => document.body.onload = callback;
 
 function $(selector, callback) {
     let query = document.querySelectorAll(`${selector}`);
-    query.forEach((element, index) => callback(element, index));
+    query.forEach((element, index, query) => callback(element, index, query));
     return true;
 }
 const $one = (selector) => document.querySelector(`${selector}`);
 
 
 const required = (value, message) => value === "" ? message : "";
-const email = (value, message) => /^[A-Za-z0-9]{3,}\@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{2,}$/g.test(value) ? "" : message
+const max = (value, range, message) => `${value}`.length > range ? message : "";
+const min = (value, range, message) => `${value}`.length < range ? message : "";
+const same = (value1, value2, message) => value1 !== value2 ? message : "";
+const email = (value, message) => /^[A-Za-z0-9]{3,}\@[a-zA-Z0-9]{3,}\.[a-zA-Z0-9]{2,}$/g.test(`${value}`) ? "" : message
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -3,7 +3,7 @@
 
 
 
-const validateInput = (element) => {
+const validateInput = (element, index, query) => {
     const errorText = element.parentNode.nextElementSibling;
     const type = element.type;
     let errors = "";
@@ -13,7 +13,39 @@ const validateInput = (element) => {
         errors = required(element.value, `${element.placeholder ?? "this field"} is required`)
         if (errors === "")
             errors = email(element.value, "invalid email address");
-    } else {
+    } else if (type === "password") {
+        let next = query[++index];
+        if (next === undefined) {
+            next = query[--index]
+        } else {
+            if (next.type === undefined)
+                next = query[--index]
+        }
+
+
+        let error2 = required(next.value, `${next.placeholder ?? "this field"} is required`);
+        if (error2 == "")
+            error2 = same(element.value, next.value, `${next.placeholder ?? "this field"} mismatch`);
+
+        const errorText2 = next.parentNode.nextElementSibling;
+
+        if (error2 !== "") {
+            errorText2.innerHTML = error2;
+            errorText2.classList.remove("error_text");
+            errorText2.classList.add("error_text-shown");
+        } else {
+            errorText2.innerHTML = "";
+            errorText2.classList.add("error_text");
+            errorText2.classList.remove("error_text-shown");
+        }
+
+
+
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+        if (errors == "")
+            errors = min(element.value, 5, `${element.placeholder ?? "this field"} must be at least 5 characters`)
+    }
+    else {
         errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
     }
     if (errors !== "") {
@@ -28,8 +60,8 @@ const validateInput = (element) => {
 }
 
 onMount(() => {
-    $(".input-control input, .input-control select", (element, index) => {
-        element.addEventListener("keydown", (ev) => validateInput(element));
+    $(".input-control input, .input-control select", (element, index, query) => {
+        element.addEventListener("keydown", (ev) => validateInput(element, index, query));
     });
 
 });
