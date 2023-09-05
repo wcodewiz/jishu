382936a17501d41ece6bde99244dcfc3cdc66aed
techwolf001 <fawasrufai999@gmail.com>
2023-09-05 12:50:34 +0100

    techwolf001

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,33 @@
 
 const validateInput = (element) => {
-    console.log(element.value)
+    const errorText = element.parentNode.nextElementSibling;
+    
+    const type = element.type;
+    let errors = "";
+    if (type === "email") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    }else if (type == "password") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`)
+        if (errors === ""){
+            errors = email(element.value, "invalid email address");
+    } else {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    }
+}
+    if (errors !== "") {
+        errorText.innerHTML = errors;
+        errorText.classList.remove("error_text");
+        errorText.classList.add("error_text-shown");
+    } else {
+        errorText.innerHTML = "";
+        errorText.classList.add("error_text");
+        errorText.classList.remove("error_text-shown");
+    }
 }
 
 onMount(() => {
     $(".login input", (element, index) => {
-        element.addEventListener("change", (ev) => validateInput(element));
+        element.addEventListener("keydown", (ev) => validateInput(element));
     });
 
 });
