a25585a1a4cb4f6c9773c2116433f720f3153750
wcodewiz <samuelclinton975@gmail.com>
2023-09-05 11:40:07 +0100

    wcodewiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,6 @@ function $(selector, callback) {
 }
 const $one = (selector) => document.querySelector(`${selector}`);
 
+
+const required = (value, message) => value === "" ? message : "";
+const email = (value, message) => /^[A-Za-z0-9]{3,}\@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{2,}$/g.test(value) ? "" : message
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,12 +1,35 @@
 
 
+
+
+
 const validateInput = (element) => {
-    alert(element.value)
+    const errorText = element.parentNode.nextElementSibling;
+    const type = element.type;
+    let errors = "";
+    if (type === "text") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    } else if (type == "email") {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`)
+        if (errors === "")
+            errors = email(element.value, "invalid email address");
+    } else {
+        errors = required(element.value, `${element.placeholder ?? "this field"} is required`);
+    }
+    if (errors !== "") {
+        errorText.innerHTML = errors;
+        errorText.classList.remove("error_text");
+        errorText.classList.add("error_text-shown");
+    } else {
+        errorText.innerHTML = "";
+        errorText.classList.add("error_text");
+        errorText.classList.remove("error_text-shown");
+    }
 }
 
 onMount(() => {
     $(".input-control input, .input-control select", (element, index) => {
-        element.addEventListener("change", (ev) => validateInput(element));
+        element.addEventListener("keydown", (ev) => validateInput(element));
     });
 
 });
