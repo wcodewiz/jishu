ff4e47d7561960c838c4ae3a3b7b4686eea41193
wcodewiz <samuelclinton975@gmail.com>
2023-09-05 10:55:33 +0100

    wcodewiz

---
+++ assets/js/signup.js
@@ -0,0 +1,13 @@
+
+
+const validateInput = (element) => {
+
+}
+
+onMount(() => {
+    $(".input-control input", (element, index) => {
+        element.addEventListener("change", (ev) => validateInput(element));
+    });
+
+});
+
